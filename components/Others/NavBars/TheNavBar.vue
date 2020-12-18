c55f23932f9a9cedef27df3517ced52d0a71ce00
Olamide Sholanke <olamidesholanke@MacBook-Pro.local>
2020-12-18 18:49:19 +0100

    feat : home page aniamtion

---
+++ components/Others/NavBars/TheNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="nav_bar">
+      <p class="nav_bar__link">Home</p>
+      <p class="nav_bar__link">About</p>
+      <p class="nav_bar__link">Work</p>
+      <p class="nav_bar__link">Reach Us</p>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.nav_bar {
+  display: flex;
+  align-items: center;
+  &__link {
+    margin-right: 20px;
+  }
+}
+</style>
